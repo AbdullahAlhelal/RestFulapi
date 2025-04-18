f963cee39cbe745975c65f7d22afe40440f32e52
Abdallh Alhelel <abdallh20hl@gmail.com>
2025-04-18 23:06:08 +0300

    add WifiScanner

--- Win32 API's/Program.cs
+++ Win32 API's/Program.cs
@@ -120,6 +120,70 @@ namespace Win32_API_s
         const int PROCESS_QUERY_INFORMATION = 0x0400;
         const int PROCESS_VM_READ = 0x0010;
 
+
+
+        public class WifiScanner
+        {
+            [DllImport("Wlanapi.dll")]
+            private static extern uint WlanOpenHandle(uint dwClientVersion , IntPtr pReserved , out uint pdwNegotiatedVersion , out IntPtr phClientHandle);
+
+            [DllImport("Wlanapi.dll")]
+            private static extern uint WlanEnumInterfaces(IntPtr hClientHandle , IntPtr pReserved , out IntPtr ppInterfaceList);
+
+            [DllImport("Wlanapi.dll")]
+            private static extern uint WlanCloseHandle(IntPtr hClientHandle , IntPtr pReserved);
+
+            [DllImport("Wlanapi.dll")]
+            private static extern uint WlanFreeMemory(IntPtr pMemory);
+
+            private IntPtr clientHandle = IntPtr.Zero;
+
+            public WifiScanner()
+            {
+                uint negotiatedVersion;
+                WlanOpenHandle(2 , IntPtr.Zero , out negotiatedVersion , out clientHandle);
+            }
+
+            ~WifiScanner()
+            {
+                WlanCloseHandle(clientHandle , IntPtr.Zero);
+            }
+
+            public List<string> GetAvailableNetworks()
+            {
+                IntPtr interfaceList = IntPtr.Zero;
+                WlanEnumInterfaces(clientHandle , IntPtr.Zero , out interfaceList);
+                var listHeader = (WlanInterfaceInfoListHeader) Marshal.PtrToStructure(interfaceList , typeof(WlanInterfaceInfoListHeader));
+                var wlanInterfaceInfo = new WlanInterfaceInfo[listHeader.dwNumberOfItems];
+                List<string> networkList = new List<string>();
+
+                for ( int i = 0 ; i < listHeader.dwNumberOfItems ; i++ )
+                {
+                    IntPtr interfaceInfoPtr = new IntPtr(interfaceList.ToInt64() + (i * Marshal.SizeOf(typeof(WlanInterfaceInfo))) + Marshal.SizeOf(typeof(int)));
+                    wlanInterfaceInfo[i] = (WlanInterfaceInfo) Marshal.PtrToStructure(interfaceInfoPtr , typeof(WlanInterfaceInfo));
+                    networkList.Add(wlanInterfaceInfo[i].strProfileName);
+                }
+
+                WlanFreeMemory(interfaceList);
+                return networkList;
+            }
+
+            [StructLayout(LayoutKind.Sequential)]
+            private struct WlanInterfaceInfoListHeader
+            {
+                public uint dwNumberOfItems;
+                public uint dwIndex;
+            }
+
+            [StructLayout(LayoutKind.Sequential , CharSet = CharSet.Unicode)]
+            private struct WlanInterfaceInfo
+            {
+                public Guid InterfaceGuid;
+                [MarshalAs(UnmanagedType.ByValTStr , SizeConst = 256)]
+                public string strProfileName;
+            }
+        }
+
         static void Main()
         {
             // The path to the wallpaper image
@@ -184,6 +248,14 @@ namespace Win32_API_s
             else
             {
                 Console.WriteLine("Failed to enumerate processes.");
+            }
+         
+
+            var wifiScanner = new WifiScanner();
+            var networks = wifiScanner.GetAvailableNetworks();
+            foreach ( var network in networks )
+            {
+                Console.WriteLine($"SSID: {network}");
             }
             Console.ReadKey();
 
